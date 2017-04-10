ec6147c3784f76801d8e7751322fdb52da426e34
jncorrea <jncorrea@utpl.edu.ec>
2017-04-09 23:00:37 -0500

    Consejos de Ahorro Total

--- vistas/calculadora/ahorroTotal.js
+++ vistas/calculadora/ahorroTotal.js
@@ -1,5 +1,27 @@
+var consumoMayores, optimizacionConsumo, porcentajeAhorroTotal;
 
-    ahorroTotal = Ext.create('Ext.panel.Panel',{
-        html: 'Ahorro Total'
-    });
-    
+consumoMayores = Ext.create('Ext.panel.Panel', {
+    cls: 'consumoTotal',
+    title: '<center><strong style="color:#003F72;">CONSUMO DE MAYORES CONSUMIDORES</strong></center>',
+    html: '<p class="valorTotal">50KWH</p>',
+    flex: 1
+});
+
+optimizacionConsumo = Ext.create('Ext.panel.Panel', {
+    cls: 'consumoTotal',
+    title: '<center><strong style="color:#003F72;">OPTIMIZACIÓN DEL CONSUMO</strong></center>',
+    html: '<p class="valorTotal">50KWH</p>',
+    flex: 1
+});
+
+porcentajeAhorroTotal = Ext.create('Ext.panel.Panel', {
+    cls: 'consumoTotal',
+    title: '<center><strong style="color:#003F72;">PORCENTAJE DE AHORRO</strong></center>',
+    html: '<p class="valorTotal">50%</p>',
+    flex: 1
+});
+
+ahorroTotal = Ext.create('Ext.panel.Panel', {
+    layout: 'hbox',
+    items: [consumoMayores, optimizacionConsumo, porcentajeAhorroTotal]
+});
