262751140827dd7bbc133ff1602f9b0d83acaa33
jncorrea <jncorrea@utpl.edu.ec>
2017-04-09 19:22:22 -0500

    grid consejos de ahorro

--- vistas/calculadora/calculoAhorro.js
+++ vistas/calculadora/calculoAhorro.js
@@ -1,13 +1,20 @@
-var consejosAhorro, panelesSolares;
+var consejosAhorro, panelesSolares, gridConsejos;
 
 Ext.onReady(function () {
     consejosAhorro = Ext.create('Ext.panel.Panel', {
         title: 'Consejos de Ahorro',
-        html: 'Consejos de Ahorro'
+        items: [gridConsejos]
     });
 
     panelesSolares = Ext.create('Ext.panel.Panel', {
         title: 'Paneles Solares',
         html: 'Paneles Solares'
+    });
+    gridConsejos = Ext.create('Ext.grid.Panel', {
+        width: 300,
+        columns: [
+            {header: 'consejo1',  width: 100},
+            {header: 'consejo2',  width: 120}
+        ]
     });
 });
